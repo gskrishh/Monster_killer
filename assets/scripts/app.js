49b38a37bc5d637f30e5210169ab9fcadbef478d
gsk769 <gskcsebu@gmail.com>
2023-05-27 16:13:39 +0100

    dynamic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,33 +1,68 @@
-const ATTACK = 14;
-const STRONG_ATTACK = 17;
+const ATTACK = 12;
+const MONSTER_ATTACK = 20;
+const STRONG_ATTACK = 25;
 const HEAL_VALUE = 20;
-let chosenMaxLife = 100;
+
+
+const enteredValue = prompt('Yentha Health kavali','100');
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+  chosenMaxLife = 100;
+}
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let attackValue;
+let hasBonusLife = true ;
+adjustHealthBars(chosenMaxLife);
+function reset(){
+    currentMonsterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+    resetGame(chosenMaxLife);
+}
+    
 
-function attackingType(mode){
+function endRound(){
+    const initialPlayerHealth = currentPlayerHealth;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK);
+    currentPlayerHealth -= playerDamage;
+    if (currentPlayerHealth<=0 && hasBonusLife){
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        setPlayerHealth(initialPlayerHealth);
+        alert("inkosari brathuku po");
+        
+    }
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+        alert("gelichav thanks namaste");
+        reset();
+    }
+    else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0){
+         alert("sava dengindi po");
+         reset();
+    
+    }    
+     else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0){
+     alert("Draw");
+     reset();
+   }
+
+}
+
+   
+   function attackingType(mode){
+    let attackValue;
     if (mode == "ATTACK"){
         attackValue = ATTACK;
     }
     else {
         attackValue = STRONG_ATTACK;
     }
-    
-   const damage = dealMonsterDamage(attackValue);
-   currentMonsterHealth -= damage;
-   const playerDamage = dealPlayerDamage(attackValue);
-   currentPlayerHealth -= playerDamage;
-   if (currentMonsterHealth <=0 && currentPlayerHealth > 0){
-    alert("you won");
+    const damage = dealMonsterDamage(attackValue);
+    currentMonsterHealth -= damage;
+    endRound();
    }
-   else if (currentPlayerHealth <=0 && currentMonsterHealth > 0){
-    alert("you lost");
-   }
-   else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0){
-    alert("Draw");
-   }
-}
 
 function attackHandler(){
     attackingType('ATTACK');   
@@ -38,7 +73,17 @@ function strongAttackHandler(){
 }
 
 function heal(){
-    increasePlayerHealth(HEAL_VALUE);                
+    let healValue;
+    if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert("You can't heal to more than your max initial health.");
+    healValue = chosenMaxLife - currentPlayerHealth;
+    } else {
+    healValue = HEAL_VALUE;
+    }
+    increasePlayerHealth(healValue);
+    currentPlayerHealth += healValue;
+    endRound();
+                  
 }
 
 
